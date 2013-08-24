6ced765ebcab8a58196796184a34fa9d74b6c595
root <root@production.ewk>
2013-08-24 19:45:35 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# karma-slimerjs-launcher
+
+> Launcher for SlimerJS.
+
+## Installation
+
+The easiest way is to keep `karma-slimerjs-launcher` as a devDependency in your `package.json`.
+```json
+{
+  "devDependencies": {
+    "karma": "~0.10",
+    "karma-slimerjs-launcher": "~0.1"
+  }
+}
+```
+
+You can simple do it by:
+```bash
+npm install karma-slimerjs-launcher --save-dev
+```
+
+## Configuration
+```js
+// karma.conf.js
+module.exports = function(config) {
+  config.set({
+    browsers: ['SlimerJS'],
+  });
+};
+```
+
+You can pass list of browsers as a CLI argument too:
+```bash
+karma start --browsers SlimerJS,Chrome
+```
+
+----
+
+For more information on Karma see the [homepage].
+
+
+[homepage]: http://karma-runner.github.com
---
+++ index.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+
+var SlimerJSBrowser = function(id, baseBrowserDecorator, logger, args, options) {
+	baseBrowserDecorator(this);
+
+	var options = args && args.options || {};
+	var log = logger.create('launcher');
+
+	this._getOptions = function(url) {
+		var self=this;
+		var command=this._getCommand();
+		// create the js file, that will open karma
+		var captureFile = self._tempDir + '/capture.js';
+		var optionsCode = Object.keys(options).map(function (key) {
+			return 'page.' + key + ' = ' + JSON.stringify(options[key]) + ';';
+		});
+		var captureCode = 'var page = require("webpage").create();\n'
+		+ optionsCode.join('\n')
+		+ '\npage.onConsoleMessage = function () {'
+		+ '\n	console.log.apply(console,'
+		+ '\n		Array.prototype.slice.call(arguments,0).forEach(function(item) {'
+		+ '\n			return JSON.stringify(item);'
+		+ '\n		})'
+		+ '\n	);'
+		+' \n};'
+		+ '\npage.open("' + url + '");\n';
+		console.log(captureCode);
+		fs.writeFileSync(captureFile, captureCode);
+
+		// and start slimerjs
+		return [captureFile];
+	};
+};
+
+
+SlimerJSBrowser.prototype = {
+	name: 'SlimerJS',
+
+	DEFAULT_CMD: {
+		linux: '/home/nfroidure/Bureau/slimerjs-0.8.2/slimerjs',
+		darwin: '/Applications/SlimerJS.app/Contents/MacOS/SlimerJS-bin',
+		win32: process.env.ProgramFiles + '\\Mozilla SlimerJS\\SlimerJS.exe'
+	},
+	ENV_CMD: 'SLIMERJSLAUNCHER'
+};
+
+SlimerJSBrowser.$inject = ['id', 'baseBrowserDecorator', 'logger', 'args', 'config.slimerjsLauncher'];
+
+// PUBLISH DI MODULE
+module.exports = {'launcher:SlimerJS': ['type', SlimerJSBrowser]};
